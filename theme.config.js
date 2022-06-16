735f3e23db6811dea9a034eaa7a06f9ffeff4968
Rulial Space <98750732+Rulial@users.noreply.github.com>
2022-06-16 12:33:57 +0000

    Update

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
+  PATHINTEGRAL: 'https://app.path-integral.com',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix:'PATH – INTEGRAL',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">PATH INTEGRAL</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        imagination > 0
       </span>
     </>
   ),
@@ -16,52 +16,52 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="PathIntegral: imagination > 0" />
+      <meta name="og:description" content="PathIntegral: imagination > 0" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="twitter:image" content="https://replit.com/@RulialSpace/documentation-starter-kitPI0#public/EHL07.png" />
+      <meta name="twitter:site:domain" content="app.path-integral.com" />
+      <meta name="twitter:url" content="https://twitter.com/pathintegral0" />
+      <meta name="og:title" content="PathIntegral: imagination > 0" />
+      <meta name="og:image" content="https://replit.com/@RulialSpace/documentation-starter-kitPI0#public/EHL07.png" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-icon-180x180.png"
+        href="/PI0MASTER.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="/android-icon-192x192.png"
+        href="/PI0MASTER.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="/PI0MASTER.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
-        href="/favicon-96x96.png"
+        href="/PI0MASTER.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="/PI0MASTER.png"
       />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta name="msapplication-TileImage" content="/PI0MASTER.png" />
     </>
   ),
   search: true,
   prevLinks: true,
   nextLinks: true,
   footer: true,
-  footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerEditLink: 'PATH INTEGRAL',
+  footerText: <>MIT {new Date().getFullYear()} © PATHINTEGRAL.</>,
   unstable_faviconGlyph: '👋',
 }
